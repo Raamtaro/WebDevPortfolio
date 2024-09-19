be215ca9daf45d1c2ae6c7f9efeff4d185b55931
Raam <raam.sanghani@gmail.com>
2024-09-19 16:44:06 -0500

    Some prelim styling setup - mobile-first and then setting media queries to size up

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import './style.css'
+import Lenis from 'lenis'
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+//   console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
